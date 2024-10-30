bb7634d1eee89ef163846bd1d709dbe7f3ae915b
ZiwenZhang <a60595677@gmail.com>
2024-10-30 13:56:25 -0400

    flask login thing

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
+from flask_login import UserMixin       
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Employee(db.Model, UserMixin):
+    __tablename__ = 'employees'
+
+    id = db.Column(db.Integer , primary_key = True)
+    name = db.Column(db.String(100), nullable=False)
+    employee_number = db.Column(db.Integer , nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
