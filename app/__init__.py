ce63683454c8325a3c0307e00320f566c1262703
ZiwenZhang <a60595677@gmail.com>
2024-10-30 17:00:03 -0400

    finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from .config import Configuration
-from .routes import orders
-from .models import db   # New import
+from .routes import orders, session
+from .models import db, Employee
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(orders.bp)
+app.register_blueprint(session.bp)
 db.init_app(app)  # Configure the application with SQLAlchemy
+
+
+login = LoginManager(app)
+login.login_view = "session.login"
+
+
+@login.user_loader
+def load_user(id):
+    return Employee.query.get(int(id))
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin       
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -11,3 +12,15 @@ class Employee(db.Model, UserMixin):
     name = db.Column(db.String(100), nullable=False)
     employee_number = db.Column(db.Integer , nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+
+
+    @property
+    def password(self):
+        return self.hashed_password
+
+    @password.setter
+    def password(self, password:str):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password:str):
+        return check_password_hash(self.password, password)
